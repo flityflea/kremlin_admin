0d35f6c33356f18da906a79bde87a1d5e0616754
balazar <flity.flea@gmail.com>
2010-06-20 19:05:45 +0100

    paginas estaticas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def Concertos
+    @title = "Concertos Agendados"
   end
 
   def Curtas
+    @title = "Noticias Curtas"
   end
 
   def Newsletter
+    @title = "Lista de Subscritores da Newsletter"
   end
 
   def Guestbook
+    @title = "Guestbook"
   end
 
   def AdicionarMedia
+    @title = "Adicionar Media"
   end
 
 end
