42814f75f8f49e493d1a18d8aad067c965cad5c2
balazar <flity.flea@gmail.com>
2010-06-20 18:54:00 +0100

    adicionei as paginas

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def Concertos
+  end
+
+  def Curtas
+  end
+
+  def Newsletter
+  end
+
+  def Guestbook
+  end
+
+  def AdicionarMedia
+  end
+
+end
